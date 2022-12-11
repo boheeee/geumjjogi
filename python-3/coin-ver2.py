468f11f0ad2b9029842042de1c31359c74b290ed
Yukyung Kim <kimyk1116@naver.com>
2022-12-11 21:09:06 +0900

    Final!

--- python-3/coin-ver2.py
+++ python-3/coin-ver2.py
@@ -1,4 +1,3 @@
-
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
@@ -9,7 +8,7 @@ src = cv2.imread('coins1.jpg')
 # Convert Image to Gray
 gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
 
-#Apply Gaussian filter
+# Apply Gaussian filter
 blr = cv2.GaussianBlur(gray, (0, 0), 1)
 
 # Circle detection
@@ -18,19 +17,12 @@ circles = cv2.HoughCircles(blr, cv2.HOUGH_GRADIENT, 1, 30, param1=130, param2=40
 sum_of_money = 0
 dst = src.copy()
 
-coin_1=0
-coin_2=0
-coin_3=0
-coin_4=0
-
-
 # Output Results
 if circles is not None:
     for i in range(circles.shape[1]):
         cx, cy, radius = circles[0][i]
-       # cv2.circle(dst, (int(cx), int(cy)), int(radius), (0, 0, 255), 2, cv2.LINE_AA)
 
-        # 동전 영역 부분 영상 추출
+        # Extract the area of the coin
         x1 = int(cx - radius)
         y1 = int(cy - radius)
         x2 = int(cx + radius)
@@ -40,39 +32,44 @@ if circles is not None:
         crop = dst[y1:y2, x1:x2, :]
         ch, cw = crop.shape[:2]
 
-        # 동전 영역에 대한 ROI 마스크 영상 생성
+        # Create ROI Mask
         mask = np.zeros((ch, cw), np.uint8)
         cv2.circle(mask, (cw//2, ch//2), radius, 255, -1)
 
-        # 동전 영역 Hue 색 성분을 +40 시프트하고, Hue 평균을 계산
         hsv = cv2.cvtColor(crop, cv2.COLOR_BGR2HSV)
         hue, _, _ = cv2.split(hsv)
         hue_shift = (hue + 40) % 180
         mean_of_hue = cv2.mean(hue_shift, mask)[0]
 
-        # Hue 평균이 90보다 작으면 10원, 90보다 크면 100원으로 간주.
+        # Classification of 10 won and 100 won
         won1 = 100
         won2 = 10
         if mean_of_hue < 90:
             cv2.circle(dst, (int(cx), int(cy)), int(radius), (0,0,255), 2, cv2.LINE_AA)
-            cv2.putText(crop, str(won2), (20,20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0,0,255), 2, cv2.LINE_AA)
+            cv2.putText(crop, str(won2), (23,25), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0,0,255), 2, cv2.LINE_AA)
             sum_of_money+=won2
+            # Circle center coordinates
+            cv2.putText(crop, f'({cx},{cy})', (3,60),cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0,0,0), 1, cv2.LINE_AA)
         else:
             cv2.circle(dst, (int(cx), int(cy)), int(radius), (255,0,0), 2, cv2.LINE_AA)
-            cv2.putText(crop, str(won1), (30,20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255,0,0), 2, cv2.LINE_AA)
+            cv2.putText(crop, str(won1), (30,30), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255,0,0), 2, cv2.LINE_AA)
             sum_of_money+=won1
+            # Circle center coordinates
+            cv2.putText(crop, f'({cx},{cy})', (15,75), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0,0,0), 1, cv2.LINE_AA)
 
+# Find the center of the circle
 if circles is not None:
     circles=np.uint16(np.around(circles))
     for i in circles[0,:]:
-        cv2.circle(dst, (i[0], i[1]), 2, (128, 206, 225), 5)
+        cv2.circle(dst, (i[0], i[1]), 2, (0, 255, 0), 5)
 
+# Design
 font={'family' : 'serif', 'color' : 'black', 'weight' : 'bold', 'size' : 20}
+title_font={'fontweight' : 'bold', 'family' : 'serif'}
 
-plt.figure(figsize=(20,20))
+plt.figure("Team 7 - geumjjogi", figsize=(20,20))
 plt.subplot(1,1,1)
-plt.title('Result', fontsize=45)
-plt.axis("off")
-plt.text(0.1, 0.1, f'sum : {sum_of_money}', fontdict=font, horizontalalignment='left', verticalalignment='bottom')
+plt.title('Result', fontsize=45, pad = 15, fontdict = title_font)
+plt.text(0.1, 0.1, f'Sum : {sum_of_money}', fontdict=font, horizontalalignment='left', verticalalignment='bottom')
 plt.imshow(dst)
 plt.show()
